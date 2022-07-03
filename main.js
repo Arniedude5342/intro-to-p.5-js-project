d728fb1eff220a6e9c876b52d3a173bb25ac4a7c
Arniedude5342 <85857848+Arniedude5342@users.noreply.github.com>
2022-07-03 12:09:48 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -12,5 +12,14 @@ function setup(){
 function draw() {
     circle(300, 30, 20);
     ellipse(49, 50, 69, 69);
+    //rect(100, 60, 100, 100);//
+    stroke('green');
+    strokeWeight(4);
     rect(100, 60, 100, 100);
+    //describe('White rect at center with green outline.');//
+
+    stroke('red');
+    strokeWeight(4);
+    circle(300, 30, 20);
+    //describe('White circle at center with red outline.');//
 }
